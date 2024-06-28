ccf72a9c089eab31c6d51a22db5137e8e0a7c384
Delhumeur <126495668+DervauxJulien@users.noreply.github.com>
2024-06-28 14:14:54 +0200

    ajout lien github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,7 +27,7 @@ const Footer = ({ nextPage , previousPage, page, setPage}) => {
   return (
       <div className='d-flex justify-content-evenly align-item-center m-3'>
         <a className='pe-5 text-decoration-none text-white' href='#'>Haut de page</a>
-        <a className='pe-5 text-decoration-none text-white' href="https://github.com/Delhumeur" target='blank'>Github</a>
+        <a className='pe-5 text-decoration-none text-white' href="https://github.com/DervauxJulien/Projet_Tmdb_ReactJs/tree/main" target='blank'>Github</a>
     </div>    
   );
 }
